16010d8d02cc697ca8961ae85498c73cd325000d
Dave Bremer <dave@bremer.co.nz>
2020-03-29 21:16:14 +1300

    added ch1\hello

---
+++ Ch1/hello.py
@@ -0,0 +1,10 @@
+#this program prints hello and asks for my name
+print("hello world")
+print('What is your name?') #ask for their name
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is:' )
+print(len(myName))
+print('what is your age?') #ask for their age
+myAge = input()
+print('you will be ' + str(int(myAge)+1)  + ' in a year.')
