5673d16f642e340705a211d97c9ca5ad0332b51b
Dave Bremer <dave@bremer.co.nz>
2020-03-30 20:09:04 +1300

    adding guessTheNumber.py

---
+++ Ch2/guessTheNumber.py
@@ -0,0 +1,26 @@
+import sys
+import random
+
+theNum = random.randint(1, 10)
+
+print("I am thinking of a number between 1 and 20.")
+match = False
+
+numGuesses = 0
+
+while not match:
+    guessing = ""    
+    while guessing == "" or not 0 < int(guessing) < 21: 
+        guessing = input("Take a guess between 1 and 20: ")
+
+    theGuess = int(guessing)
+    numGuesses += 1
+
+    if theNum == theGuess:
+        match = True
+        print("Good job! You guessed my number in " + str(numGuesses) + " guesses!")
+
+    elif theGuess < theNum:
+        print("Your guess is too low")
+    else:
+        print("Your guess is too high")
